a5e373c5ebfbccec6571c7d499a4a3a9c6b14675
Thawan Camara <thawancamara@gmail.com>
2022-02-15 17:13:02 -0300

    improved create person code, added mapstruct to dependencies

--- src/main/java/one/digitalinnovation/services/PersonService.java
+++ src/main/java/one/digitalinnovation/services/PersonService.java
@@ -2,10 +2,11 @@ package one.digitalinnovation.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entities.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.IPersonRepository;
 
 @Service
@@ -13,14 +14,17 @@ public class PersonService {
 	
 	private IPersonRepository personRepository;
 	
+	private final PersonMapper personMapper = PersonMapper.INSTANCE;
+	
 	@Autowired
 	public PersonService(IPersonRepository personRepository) {
 		this.personRepository = personRepository; 
-	}
+	}	
 	
-	
-	public MessageResponseDTO createPerson(Person person) {
-		Person savedPerson = personRepository.save(person);
+	public MessageResponseDTO createPerson(PersonDTO personDTO) {
+		Person personToSave = personMapper.toModel(personDTO);		
+
+		Person savedPerson = personRepository.save(personToSave);
 		return MessageResponseDTO
 				.builder()
 				.message("Created person with ID " + savedPerson.getId())
